fd1c3ceb87b12e25ebddad786482f1d7708c3a78
thinhhja2001 <19522281@gm.uit.edu.vn>
2022-06-14 23:30:54 +0700

    update api

--- lib/widgets/music/song/list_song_widget.dart
+++ lib/widgets/music/song/list_song_widget.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:musix/resources/song_methods.dart';
 import 'package:musix/utils/colors.dart';
+import 'package:musix/utils/constant.dart';
 import 'package:musix/widgets/music/song/music_selection_widget.dart';
 
 class ListSongWidget extends StatelessWidget {
@@ -15,18 +16,27 @@ class ListSongWidget extends StatelessWidget {
       future: SongMethods.getListSongDataByKeys(songsKey),
       builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
         if (snapshot.hasData) {
-          return Column(
-            children: [
-              ListView.builder(
-                  shrinkWrap: true,
-                  physics: const NeverScrollableScrollPhysics(),
-                  itemCount: snapshot.data.length,
-                  itemBuilder: (context, position) => MusicSelectionWidget(
-                        index: position,
-                        song: snapshot.data[position],
-                      )),
-            ],
-          );
+          if (snapshot.data.length == 0) {
+            return const Center(
+              child: Text(
+                "Listen history is empty.",
+                style: kDefaultTextStyle,
+              ),
+            );
+          } else {
+            return Column(
+              children: [
+                ListView.builder(
+                    shrinkWrap: true,
+                    physics: const NeverScrollableScrollPhysics(),
+                    itemCount: snapshot.data.length,
+                    itemBuilder: (context, position) => MusicSelectionWidget(
+                          index: position,
+                          song: snapshot.data[position],
+                        )),
+              ],
+            );
+          }
         } else {
           return const Center(
             child: CircularProgressIndicator(
