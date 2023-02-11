53804b075915200b5f262e96d15c54a3cc8df1ba
Minh Quan <minhquancn1809@gmail.com>
2023-02-12 00:08:51 +0700

    120223 update search ui - hot fix

--- lib/domain_album/views/widgets.dart
+++ lib/domain_album/views/widgets.dart
@@ -1,3 +1,4 @@
-export 'widgets/weekly_album_widget.dart';
+export 'widgets/album_selection_widget.dart';
 export 'widgets/new_album_widget.dart';
 export 'widgets/search_album_widget.dart';
+export 'widgets/weekly_album_widget.dart';
